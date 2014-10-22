4444d339478e80df3910df945845766bf4f824a3
CarmenGraus <carmengraus95@gmail.com>
2014-10-22 18:57:14 +0200

    Cambios 3º Día
    Fallo en Script de texto

--- Assets/Scripts/scoreUp.cs
+++ Assets/Scripts/scoreUp.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class scoreUp : MonoBehaviour {
 
+	public class scoreUp : MonoBehaviour
+		 
+		private Text texto;
+	void Start() {
+		texto = GameObject.Find ("TextoNivel").GetComponent<Text>();
+	}
+
+
 	void OnTriggerEnter2D(Collider2D col){
 		GameControl.score = GameControl.score + 1;
-		Debug.Log (GameControl.score);
+
+		texto.text = "Level" + GameControl.score.ToString ();
 }
 }
